d6417ed4be00e8ac62b0b393edc416acdc898892
Timothy Halley <timothy.halley@gmail.com>
2017-06-14 07:25:27 -0700

    Coffee missing

--- js/skicyclerun.js
+++ js/skicyclerun.js
@@ -1,3 +1,12 @@
 /*
 	Custom Function for skicyclerun.com
 */
+
+//ScrollMagic - create the controller
+var controller = new ScrollMagic.Controller();
+
+//build a scene
+var pinScene = new ScrollMagic.Scene({
+     triggerElement: "#pin-me",
+     duration: 300
+})
