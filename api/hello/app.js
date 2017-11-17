5e1d2cc6a64a2d36566664f34f86154d18559e5a
Tim Halley <timothy.halley@gmail.com>
2017-11-17 06:11:49 -0800

    Thrusday

--- api/hello/app.js
+++ api/hello/app.js
@@ -5,5 +5,5 @@ module.exports = api;
 
 api.get('/greet', function(request) {
   var superb = require('superb');
-  return request.queryString.name + ', this is a load of crap and not really too fun bozo! Ya think! --> ' + superb();
+  return request.queryString.name + ', At work today! --> ' + superb();
 });
--- api/resize/app.js
+++ api/resize/app.js
@@ -1,16 +1,42 @@
 var ApiBuilder = require('claudia-api-builder');
 var api = new ApiBuilder();
-
-module.exports = api;
-
-const AWS = require('aws-sdk');
-const S3 = new AWS.S3({
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-west-2'});
+var Sharp = require('sharp');
+var BUCKET = process.env.BUCKET;
+var URL = process.env.URL;
+var S3 = new AWS.S3({
   signatureVersion: 'v4',
 });
 
-const Sharp = require('sharp');
+module.exports = api;
 
-api.get('/greet', function(request) {
-  var superb = require('superb');
-  return request.queryString.name + ', this is a load of crap and not really too fun bozo! Ya think! --> ' + superb();
-});
+exports.handler = function(event, context, callback) {
+  const key = event.queryStringParameters.key;
+
+  const match = key.match(/(\d+)x(\d+)\/(.*)/);
+  const width = parseInt(match[1], 10);
+  const height = parseInt(match[2], 10);
+  const originalKey = match[3];
+
+  S3.getObject({Bucket: BUCKET, Key: originalKey}).promise()
+    .then(data => Sharp(data.Body)
+      .resize(width, height)
+      .toFormat('png')
+      .toBuffer()
+    )
+    .then(buffer => S3.putObject({
+        Body: buffer,
+        Bucket: BUCKET,
+        ContentType: 'image/png',
+        Key: key,
+      }).promise()
+    )
+    .then(() => callback(null, {
+        statusCode: '301',
+        headers: {'location': `${URL}/${key}`},
+        body: '',
+      })
+    )
+    .catch(err => callback(err))
+};
