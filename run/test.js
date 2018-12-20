af79b13360aabb954723e0e683c2007670463fa2
timothy halley <skicyclerun@gmail.com>
2018-12-20 14:46:58 -0800

    V3.0 - more on localhost test.js

--- run/test.js
+++ run/test.js
@@ -1,9 +1,20 @@
 //https://dev.to/aurelkurtula/creating-a-basic-website-with-expressjs-j92
 //import express from 'express';
+
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+
 const express = require('express');
-const app = express()
+const app = express();
+app.set('view engine', 'ejs');
+
+app.use(express.static('../'))
+app.use(express.static('../css'))
+app.use(express.static('../js'))
+
 app.get('/', (req, res) => {
-    res.send('../default.html')
+    res.send(alpha)
 })
 app.listen(8080, () => {
     console.log('http://localhost:8080')
