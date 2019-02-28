4bdf338cffde860416934e811bf454a3b19a9c98
Timothy Halley <timothy.halley@gmail.com>
2019-02-28 05:31:13 -0800

    V3.7 - getting there...

--- run/js/alpha.js
+++ run/js/alpha.js
@@ -18,6 +18,7 @@ $('.grid-item').click(function(event) {
 
   if (!$(this).hasClass('item-opened')) {
     console.log('DEBUG: !NOT has class --> item-opened')
+
     // Values
     var elWidth = $(this).outerWidth() / 2;
     var elPosition = this.getBoundingClientRect();
@@ -25,6 +26,9 @@ $('.grid-item').click(function(event) {
     // Store position
     $(this).attr('data-coord-left', $(this).css('left'));
     $(this).attr('data-coord-top', $(this).css('top'));
+
+    // Update MAP
+    selectMap($(this).css('background-image'));
 
     // Transition effect
     $(this).css({
@@ -66,9 +70,6 @@ $('.grid-item').click(function(event) {
       scrollTop: $('.grid').offset().top
     }, 650);
     $('.grid').css('overflow', 'visible');
-
-    selectMap($(this).css('background-image'));
-
 
   } else {
 
@@ -122,18 +123,24 @@ $(document).on('click', function(e) {
 
 });
 
+const alpha = document.querySelector(".map-alpha");
+alpha.addEventListener('dblclick', function (e) {
+
+  // change backgroud image? // Keep alpha to 300 rnd #
+  const rndNo = getRandomInt(10000, 10300);
+  const newURL = 'https://img.skicyclerun.com/pub/skiCycleRun/' + rndNo + '.jpg';
+  $(this).css('background-image', 'url("' + newURL + '")');
+  console.log('DEBUG: new Background --> ', newURL)
+
+  $('.grid-alpha').fadeOut(650);
+
+});
+
 $(document).on('click', function(e) {
 
-  console.log('EVENT: --> CLICK CLICK!');
+  console.log('EVENT: --> getting location');
 
-  //getLocation();
-
-  //
-  //  change backgroud image? // Keep alpha to 300 rnd #
-  // const rndNo = getRandomInt(10000, 10300);
-  // const newURL = 'https://img.skicyclerun.com/pub/skiCycleRun/' + rndNo + '.jpg';
-  // $(this).css('background-image', 'url("' + newURL + '")');
-  // console.log('DEBUG: new Background --> ', newURL)
+  // getLocation();
 
 });
 
@@ -218,213 +225,3 @@ function getLocation() {
     alert("Sorry, browser does not support geolocation!");
   }
 }
-
-
-//----------- Modernizr ------
-addEventListener(document, "touchstart", function(e) {
-  console.log('DEBUG: ', e.defaultPrevented); // will be false
-  e.preventDefault(); // does nothing since the listener is passive
-  console.log('DEBUG: ', e.defaultPrevented); // still false
-}, Modernizr.passiveeventlisteners ? {
-  passive: true
-} : false);
-
-/*! modernizr 3.6.0 (Custom Build) | MIT *
- * https://modernizr.com/download/?-flexbox-passiveeventlisteners-scriptasync-setclasses !*/
-! function(e, n, t) {
-  function r(e, n) {
-    return typeof e === n
-  }
-
-  function s() {
-    var e, n, t, s, o, i, a;
-    for (var l in S)
-      if (S.hasOwnProperty(l)) {
-        if (e = [], n = S[l], n.name && (e.push(n.name.toLowerCase()), n.options && n.options.aliases && n.options.aliases.length))
-          for (t = 0; t < n.options.aliases.length; t++) e.push(n.options.aliases[t].toLowerCase());
-        for (s = r(n.fn, "function") ? n.fn() : n.fn, o = 0; o < e.length; o++) i = e[o], a = i.split("."), 1 === a.length ? Modernizr[a[0]] = s : (!Modernizr[a[0]] || Modernizr[a[0]] instanceof Boolean || (Modernizr[a[0]] = new Boolean(Modernizr[a[0]])), Modernizr[a[0]][a[1]] = s), C.push((s ? "" : "no-") + a.join("-"))
-      }
-  }
-
-  function o(e) {
-    var n = x.className,
-      t = Modernizr._config.classPrefix || "";
-    if (_ && (n = n.baseVal), Modernizr._config.enableJSClass) {
-      var r = new RegExp("(^|\\s)" + t + "no-js(\\s|$)");
-      n = n.replace(r, "$1" + t + "js$2")
-    }
-    Modernizr._config.enableClasses && (n += " " + t + e.join(" " + t), _ ? x.className.baseVal = n : x.className = n)
-  }
-
-  function i() {
-    return "function" != typeof n.createElement ? n.createElement(arguments[0]) : _ ? n.createElementNS.call(n, "http://www.w3.org/2000/svg", arguments[0]) : n.createElement.apply(n, arguments)
-  }
-
-  function a(e, n) {
-    return !!~("" + e).indexOf(n)
-  }
-
-  function l(e) {
-    return e.replace(/([a-z])-([a-z])/g, function(e, n, t) {
-      return n + t.toUpperCase()
-    }).replace(/^-/, "")
-  }
-
-  function f(e, n) {
-    return function() {
-      return e.apply(n, arguments)
-    }
-  }
-
-  function u(e, n, t) {
-    var s;
-    for (var o in e)
-      if (e[o] in n) return t === !1 ? e[o] : (s = n[e[o]], r(s, "function") ? f(s, t || n) : s);
-    return !1
-  }
-
-  function c(e) {
-    return e.replace(/([A-Z])/g, function(e, n) {
-      return "-" + n.toLowerCase()
-    }).replace(/^ms-/, "-ms-")
-  }
-
-  function d(n, t, r) {
-    var s;
-    if ("getComputedStyle" in e) {
-      s = getComputedStyle.call(e, n, t);
-      var o = e.console;
-      if (null !== s) r && (s = s.getPropertyValue(r));
-      else if (o) {
-        var i = o.error ? "error" : "log";
-        o[i].call(o, "getComputedStyle returning null, its possible modernizr test results are inaccurate")
-      }
-    } else s = !t && n.currentStyle && n.currentStyle[r];
-    return s
-  }
-
-  function p() {
-    var e = n.body;
-    return e || (e = i(_ ? "svg" : "body"), e.fake = !0), e
-  }
-
-  function m(e, t, r, s) {
-    var o, a, l, f, u = "modernizr",
-      c = i("div"),
-      d = p();
-    if (parseInt(r, 10))
-      for (; r--;) l = i("div"), l.id = s ? s[r] : u + (r + 1), c.appendChild(l);
-    return o = i("style"), o.type = "text/css", o.id = "s" + u, (d.fake ? d : c).appendChild(o), d.appendChild(c), o.styleSheet ? o.styleSheet.cssText = e : o.appendChild(n.createTextNode(e)), c.id = u, d.fake && (d.style.background = "", d.style.overflow = "hidden", f = x.style.overflow, x.style.overflow = "hidden", x.appendChild(d)), a = t(c, e), d.fake ? (d.parentNode.removeChild(d), x.style.overflow = f, x.offsetHeight) : c.parentNode.removeChild(c), !!a
-  }
-
-  function v(n, r) {
-    var s = n.length;
-    if ("CSS" in e && "supports" in e.CSS) {
-      for (; s--;)
-        if (e.CSS.supports(c(n[s]), r)) return !0;
-      return !1
-    }
-    if ("CSSSupportsRule" in e) {
-      for (var o = []; s--;) o.push("(" + c(n[s]) + ":" + r + ")");
-      return o = o.join(" or "), m("@supports (" + o + ") { #modernizr { position: absolute; } }", function(e) {
-        return "absolute" == d(e, null, "position")
-      })
-    }
-    return t
-  }
-
-  function y(e, n, s, o) {
-    function f() {
-      c && (delete T.style, delete T.modElem)
-    }
-    if (o = r(o, "undefined") ? !1 : o, !r(s, "undefined")) {
-      var u = v(e, s);
-      if (!r(u, "undefined")) return u
-    }
-    for (var c, d, p, m, y, g = ["modernizr", "tspan", "samp"]; !T.style && g.length;) c = !0, T.modElem = i(g.shift()), T.style = T.modElem.style;
-    for (p = e.length, d = 0; p > d; d++)
-      if (m = e[d], y = T.style[m], a(m, "-") && (m = l(m)), T.style[m] !== t) {
-        if (o || r(s, "undefined")) return f(), "pfx" == n ? m : !0;
-        try {
-          T.style[m] = s
-        } catch (h) {}
-        if (T.style[m] != y) return f(), "pfx" == n ? m : !0
-      } return f(), !1
-  }
-
-  function g(e, n, t, s, o) {
-    var i = e.charAt(0).toUpperCase() + e.slice(1),
-      a = (e + " " + P.join(i + " ") + i).split(" ");
-    return r(n, "string") || r(n, "undefined") ? y(a, n, s, o) : (a = (e + " " + E.join(i + " ") + i).split(" "), u(a, n, t))
-  }
-
-  function h(e, n, r) {
-    return g(e, t, t, n, r)
-  }
-  var C = [],
-    S = [],
-    w = {
-      _version: "3.6.0",
-      _config: {
-        classPrefix: "",
-        enableClasses: !0,
-        enableJSClass: !0,
-        usePrefixes: !0
-      },
-      _q: [],
-      on: function(e, n) {
-        var t = this;
-        setTimeout(function() {
-          n(t[e])
-        }, 0)
-      },
-      addTest: function(e, n, t) {
-        S.push({
-          name: e,
-          fn: n,
-          options: t
-        })
-      },
-      addAsyncTest: function(e) {
-        S.push({
-          name: null,
-          fn: e
-        })
-      }
-    },
-    Modernizr = function() {};
-  Modernizr.prototype = w, Modernizr = new Modernizr, Modernizr.addTest("passiveeventlisteners", function() {
-    var n = !1;
-    try {
-      var t = Object.defineProperty({}, "passive", {
-        get: function() {
-          n = !0
-        }
-      });
-      e.addEventListener("test", null, t)
-    } catch (r) {}
-    return n
-  });
-  var x = n.documentElement,
-    _ = "svg" === x.nodeName.toLowerCase();
-  Modernizr.addTest("scriptasync", "async" in i("script"));
-  var b = "Moz O ms Webkit",
-    P = w._config.usePrefixes ? b.split(" ") : [];
-  w._cssomPrefixes = P;
-  var E = w._config.usePrefixes ? b.toLowerCase().split(" ") : [];
-  w._domPrefixes = E;
-  var z = {
-    elem: i("modernizr")
-  };
-  Modernizr._q.push(function() {
-    delete z.elem
-  });
-  var T = {
-    style: z.elem.style
-  };
-  Modernizr._q.unshift(function() {
-    delete T.style
-  }), w.testAllProps = g, w.testAllProps = h, Modernizr.addTest("flexbox", h("flexBasis", "1px", !0)), s(), o(C), delete w.addTest, delete w.addAsyncTest;
-  for (var N = 0; N < Modernizr._q.length; N++) Modernizr._q[N]();
-  e.Modernizr = Modernizr
-}(window, document);
